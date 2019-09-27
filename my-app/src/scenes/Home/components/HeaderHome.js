eeaf5292963d70c9207d1618ff927a91a164e41f
Ronaldjeq <1421668@utp.edu.pe>
2019-09-27 11:37:30 -0500

    added header

---
+++ my-app/src/scenes/Home/components/HeaderHome.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+import { Row, Col,Button, Icon } from 'antd';
+import './HeaderHome.css'
+const BlueMenu = ({ handleLogout }) => (
+    <div id="headerHome">
+
+        <Row className='headerRow' type="flex" align="middle" type="flex">
+            <Col push={2} span={8}>
+                <img className='imageHeader' src="/logo-comercio.png"  />    
+            </Col>
+            <Col span={4} push={12}>
+                <Button  shape="circle" >
+                    <Icon type="logout" />
+                </Button>
+            </Col>
+        </Row>
+    </div>
+);
+
+export default BlueMenu;
--- my-app/src/scenes/Home/index.js
+++ my-app/src/scenes/Home/index.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch,Redirect, Route } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-
+import { Layout, Breadcrumb, Alert  } from 'antd';
+import HeaderHome from './components/HeaderHome'
 import history from '../../services/history';
 
 class Home extends Component {
@@ -15,18 +16,61 @@ class Home extends Component {
   static propTypes = {
     dispatch: PropTypes.func,
     token: PropTypes.string,
+    pathName: PropTypes.string,
   };
 
 
 
   render() {
     const { Sessiontoken } =this.state;
+    const {pathName}=this.props
     if (!Sessiontoken) {
       history.push('/login');
     }
     return (
       <div id="home">
-        <span>holi</span>
+      <HeaderHome/>
+        <Route exact path={pathName} render={()=><Redirect to="/alumnos"/>}/>
+            <Breadcrumb separator=">">
+                <Breadcrumb.Item   >
+                  <span className='homeLink'  onClick={() => history.push('/')}>Inicio</span>
+                </Breadcrumb.Item>
+              <Breadcrumb.Item>Obtener Reporte</Breadcrumb.Item>
+            </Breadcrumb>
+            {/* <Layout style={{ padding: '24px 0', background: '#fff' }}>
+                  <Sider width={200} style={{ background: '#fff' }}>
+                    <MenuReport />
+                  </Sider>
+                  <Content style={{ padding: '0 24px', minHeight: 280 }}>
+                      <Switch 
+                      >
+                        <Route
+                          path={`${pathName}/alumnos`}
+                          render={ () => <StudentsReport /> }
+                        />
+                        <Route
+                          path={`${pathName}/finanzas`}
+                          render={() => <FinanceReport />}
+                        />
+                        <Route
+                          path={`${pathName}/cuentas`}
+                          render={() => <StudentsReport2 />}
+                        />
+                        <Route
+                          path={`${pathName}/rrhh`}
+                          render={() => <StudentsReport2 />}
+                        />
+                        <Route
+                          path={`${pathName}/general`}
+                          render={() => <StudentsReport2 />}
+                        />
+                        <Route
+                          path={`${pathName}/favoritos`}
+                          render={() => <StudentsReport2 />}
+                        />
+                      </Switch>
+                  </Content>
+            </Layout> */}
       </div>
     );
   }
